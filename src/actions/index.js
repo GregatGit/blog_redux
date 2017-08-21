5c173a2b05820b787e493bdd6924cd2325313a30
Greg Duncan <gregdd@outlook.com>
2017-08-21 23:04:01 +1000

    add Link from react-router-dom

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,6 @@ const ROOT_URL = 'http://reduxblog.herokuapp.com/api'
 const API_KEY = '?key=Me12333'
 
 export function fetchPosts () {
-  console.log('fetching!')
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`)
   return {
     type: FETCH_POSTS,
