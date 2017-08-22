73fa6ec2da073c19c91b00ef5d5ed1879f4d5d91
Greg Duncan <gregdd@outlook.com>
2017-08-22 20:42:01 +1000

    added form functionality

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,29 +1,75 @@
 import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
+import { Link } from 'react-router-dom'
 
 class PostNew extends Component {
-  renderTitleField (field) {
+  renderField (field) {
+    const { meta: { touched, error } } = field
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`
     return (
-      <div>
+      <div className={className}>
+      <label>{field.label}</label>
         <input
+          className='form-control'
           type='text'
           {...field.input}
         />
+        <div className='text-help'>
+          {touched ? error : ''}
+        </div>
       </div>
     )
   }
+  onSubmit (values) {
+    console.log(values)
+  }
+
   render () {
+    const { handleSubmit } = this.props
     return (
-      <form>
-        <Field 
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <Field
+          label='Title'
           name='title'
-          component={this.renderTitleField}
+          component={this.renderField}
         />
+        <Field
+          label='Categories'
+          name='categories'
+          component={this.renderField}
+        />
+        <Field 
+          label='Post Content'
+          name='content'
+          component={this.renderField}
+        />
+        <button type='submit' className='btn btn-primary'> Submit</button>
+        <Link to='/' className='btn btn-danger'>Cancel</Link>
       </form>
     )
   }
 }
 
+function validate(values) {
+  const errors = {}
+
+  // Validate the inputs from 'values'
+  if (!values.title) {
+    errors.title = 'Enter a title!'
+  }
+  if (!values.categories) {
+    errors.categories = 'Enter some categories!'
+  }
+  if (!values.content) {
+    errors.content = 'You must enter some content'
+  }
+
+  // If errors is empty - the form is fine to submit
+  // if errors has any properties - redux form assumes form is invalid
+  return errors
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostNew)
