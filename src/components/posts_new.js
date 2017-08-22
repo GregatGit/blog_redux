011db76ed032f0bfdd963c32c848c99667130411
Greg Duncan <gregdd@outlook.com>
2017-08-22 11:39:42 +1000

    added redux forms

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react'
+import { Field, reduxForm } from 'redux-form'
 
 class PostNew extends Component {
-  render () {
+  renderTitleField (field) {
     return (
       <div>
-        Post new!
+        <input
+          type='text'
+          {...field.input}
+        />
       </div>
+    )
+  }
+  render () {
+    return (
+      <form>
+        <Field 
+          name='title'
+          component={this.renderTitleField}
+        />
+      </form>
     )
   }
 }
 
-export default PostNew
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostNew)
