715ba77e003559a4eb72c7ba11edc4014dbe0cce
Greg Duncan <gregdd@outlook.com>
2017-08-22 21:07:42 +1000

    Post new entry to DB

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,8 @@
-export const FETCH_POSTS = 'fetch_posts'
 import axios from 'axios'
+
+export const FETCH_POSTS = 'fetch_posts'
+export const CREATE_POST = 'create_post'
+
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api'
 const API_KEY = '?key=Me12333'
 
@@ -7,6 +10,15 @@ export function fetchPosts () {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`)
   return {
     type: FETCH_POSTS,
+    payload: request
+  }
+}
+
+export function createPost(values) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
+
+  return {
+    type: CREATE_POST,
     payload: request
   }
 }
